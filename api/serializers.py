eca4ce799bd6814e63c48acd34eae89dcd8b0c42
bernardoodp <bdavipandolfo@gmail.com>
2025-12-22 11:29:36 -0300

    feat: changing value convertion in serializer for organization

--- api/serializers.py
+++ api/serializers.py
@@ -7,10 +7,8 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
 
     def to_representation(self, instance):
-        # Pega a representação original (o dicionário com os dados)
         data = super().to_representation(instance)
         
-        # Altera o preço dividindo por 100
         data['price'] = instance.price / 100
         
         return data
